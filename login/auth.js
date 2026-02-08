9eed713fb68f1f6011ee2e78dca4561d997de1f9
Lucas Pereira Esteves <100858060+foter1234@users.noreply.github.com>
2026-02-08 12:11:47 -0300

    criando pagina de cadatro com supaBase

---
+++ login/auth.js
@@ -0,0 +1,38 @@
+const supabaseUrl = "https://uqhuqyyzygzisohwkyeu.supabase.co";
+const supabaseKey = "sb_publishable_JxPXj6Vyt7z_Jj_SPofMOw_Gsmk3hMW";
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+async function register() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const msg = document.getElementById("msg");
+
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password
+  });
+
+  if (error) {
+    msg.textContent = error.message;
+  } else {
+    msg.textContent = "Conta criada! Verifique seu e-mail.";
+  }
+}
+
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const msg = document.getElementById("msg");
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password
+  });
+
+  if (error) {
+    msg.textContent = error.message;
+  } else {
+    msg.textContent = "Login realizado!";
+    window.location.href = "home.html";
+  }
+}
